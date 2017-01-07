6c5a1d8654d05f7b5cc76684bc0dbd76602c7c2e
Manu Viswam <evmanu@gmail.com>
2017-01-07 12:07:27 +0530

    Manu - Timeout search after 1 second

--- handler/search.go
+++ handler/search.go
@@ -11,32 +11,65 @@ import (
 	s "github.com/manuviswam/multisearch/service"
 )
 
+const (
+	SearchTImeoutInSeconds = 1
+	SearchParameterKey     = "q"
+)
+
 func HandleSearch(google *s.GoogleSearch, duckDuckGo *s.DuckDuckGoSearch, twitter *s.TwitterSearch) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
-		query := r.URL.Query().Get("q")
+		query := r.URL.Query().Get(SearchParameterKey)
 		encodedQuery := url.QueryEscape(query)
 
-		c1 := make(chan m.SearchResult)
-		c2 := make(chan m.SearchResult)
-		c3 := make(chan m.SearchResult)
-
-		go google.Search(encodedQuery, c1)
-		go duckDuckGo.Search(encodedQuery, c2)
-		go twitter.Search(encodedQuery, c3)
+		googleChannel := searchWithTimeout(encodedQuery, google.Search)
+		duckDuckGoChannel := searchWithTimeout(encodedQuery, duckDuckGo.Search)
+		twitterChannel := searchWithTimeout(encodedQuery, twitter.Search)
+		defer func() {
+			close(googleChannel)
+			close(duckDuckGoChannel)
+			close(twitterChannel)
+		}()
 
 		response := m.SearchResponse{
 			Query: query,
 			Results: m.SearchResults{
-				Google:     <-c1,
-				Duckduckgo: <-c2,
-				Twitter:    <-c3,
+				Google:     <-googleChannel,
+				Duckduckgo: <-duckDuckGoChannel,
+				Twitter:    <-twitterChannel,
 			},
 		}
+
 		jsonResponse, _ := json.Marshal(response)
+		w.Header().Add("Content-Type", "application/json")
 		w.Write(jsonResponse)
 
 		fmt.Println("Elapsed time ", time.Since(start))
 	}
+}
+
+func searchWithTimeout(query string, search func(string, chan m.SearchResult)) chan m.SearchResult {
+	c := make(chan m.SearchResult)
+	go search(query, c)
+
+	timeout := make(chan bool)
+	go func() {
+		time.Sleep(SearchTImeoutInSeconds * time.Second)
+		timeout <- true
+	}()
+
+	outChannel := make(chan m.SearchResult)
+	go func() {
+		select {
+		case result := <-c:
+			outChannel <- result
+		case <-timeout:
+			outChannel <- m.SearchResult{
+				Error: "Search timed out",
+			}
+		}
+	}()
+
+	return outChannel
 }
